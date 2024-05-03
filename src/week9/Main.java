a1a84c5ef751f00cc84b9813ebae97e6f7e7f6f6
jiwonpark831 <jiwonpark23@handong.ac.kr>
2024-05-03 14:08:04 +0900

    week 9

---
+++ src/week9/Main.java
@@ -0,0 +1,15 @@
+package week9;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        Main pStudio = new Main();
+        pStudio.J041();
+    }
+
+    void J041(){
+
+    }
+}
